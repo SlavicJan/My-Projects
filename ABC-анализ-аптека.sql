1e0dcb622b7bc7595ce36a7196a3f91a74e2324f
SlavicJan <svyatoslav263@gmail.com>
2025-06-25 17:56:10 +0300

    sql abc n-day

---
+++ ABC-анализ-аптека.sql
@@ -0,0 +1,43 @@
+﻿-- Создаём временную таблицу по товарам
+with abc_sales as (
+    select
+        dr_ndrugs as product, -- название или код лекарства
+        sum(dr_kol) as amount, -- общее количество продаж
+        sum(dr_kol*(dr_croz - dr_czak) - dr_sdisc) as profit, -- общая прибыль: объём * (розничная цена - закупочная) минус скидки
+        sum(dr_kol*dr_croz - dr_sdisc) as revenue -- общая выручка: объём * розничная цена минус скидки
+    from sales s
+    group by dr_ndrugs -- группировка по продукту
+)
+
+
+–классификация по ABC-анализу
+select
+    s.product,
+
+
+    -- ABC по количеству (amount)
+    case
+        when sum(amount) over(order by amount desc) / sum(amount) over() <= 0.8 then 'A' -- топ 80% объёма
+        when sum(amount) over(order by amount desc) / sum(amount) over() <= 0.95 then 'B' -- следующие 15%
+        else 'C' -- оставшиеся 5%
+    end amount_ABC,
+
+
+    -- ABC по прибыли
+    case
+        when sum(profit) over(order by profit desc) / sum(profit) over() <= 0.8 then 'A'
+        when sum(profit) over(order by profit desc) / sum(profit) over() <= 0.95 then 'B'
+        else 'C'
+    end profit_ABC,
+
+
+    -- ABC по выручке
+    case
+        when sum(revenue) over(order by revenue desc) / sum(revenue) over() <= 0.8 then 'A'
+        when sum(revenue) over(order by revenue desc) / sum(revenue) over() <= 0.95 then 'B'
+        else 'C'
+    end revenue_ABC
+
+
+from abc_sales s
+order by product -- сортировка по названию продукта
---
+++ когортный анализ N-day retention польз.sql
@@ -0,0 +1,37 @@
+﻿with user_entry_and_register_info as (
+  -- Формируем сводную таблицу с информацией о посещениях,
+  -- датой регистрации и разницей в днях
+  select
+    u.user_id,
+    date(u.entry_at) as entry_at,
+    date(u2.date_joined) as date_joined,
+    extract(days from u.entry_at - u2.date_joined) as diff,
+    to_char(u2.date_joined, 'MM') as cohort
+  from userentry u
+  join users u2
+  on u.user_id = u2.id
+  where to_char(u2.date_joined, 'YYYY') = '2022'
+),
+count_users_by_cohort_and_activity as (
+  -- Считаем количество пользователей с активностью
+  -- в N-дни с разбивкой по когортам
+  select
+      cohort,
+       diff,
+       count(distinct user_id) as cnt
+  from user_entry_and_register_info
+  where diff in (0, 1, 3, 7, 14, 30)
+  group by cohort, diff
+)
+-- Собираем результат в сводную wide table
+select
+  to_char(to_date(cohort, 'MM'), 'Month') as cohort,
+  max(case when diff = 0 then cnt end) as "Day 0",
+  max(case when diff = 1 then cnt end) as "Day 1",
+  max(case when diff = 3 then cnt end) as "Day 3",
+  max(case when diff = 7 then cnt end) as "Day 7",
+  max(case when diff = 14 then cnt end) as "Day 14",
+  max(case when diff = 30 then cnt end) as "Day 30"
+from count_users_by_cohort_and_activity
+group by cohort;
+
